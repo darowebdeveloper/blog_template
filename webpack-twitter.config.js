a104b00e28532b1c27025f368e198bea1aa96f80
Daro Webdeveloper <darowebdeveloper@gmail.com>
2021-04-30 10:23:14 +0700

    finished twitter main page

---
+++ webpack-twitter.config.js
@@ -0,0 +1,139 @@
+const currentTask = process.env.npm_lifecycle_event; //return dev or build npm run script
+const path = require('path');
+// plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+  context: path.resolve(__dirname, 'twitter'),
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/env'],
+            // plugins: ['transform-class-properties'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Index page generated',
+      filename: 'index.html',
+      template: './pages/index.html',
+      chunks: ['index'], // from entry name
+    }),
+  ],
+};
+
+if (currentTask == 'dev-sandbox') {
+  config.entry = {
+    index: './index.js',
+  };
+  config.output = {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'twitter/dist'),
+    clean: {
+      dry: true,
+    },
+  };
+  config.devServer = {
+    contentBase: path.join(__dirname, 'twitter/dist'),
+    index: 'sandbox.html',
+    port: 9000,
+  };
+  config.mode = 'development';
+  config.module.rules.push(
+    {
+      test: /\.(css)$/,
+      use: ['style-loader', 'css-loader'],
+      // use: [MiniCssExtractPlugin.loader, 'css-loader'],
+    },
+    {
+      test: /\.(scss)$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      // use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+    }
+  );
+  config.plugins = [
+    new HtmlWebpackPlugin({
+      filename: 'sandbox.html',
+      template: './pages/sandbox.html',
+      chunks: ['index'], // from entry name
+    }),
+  ];
+}
+
+if (currentTask == 'dev-twitter') {
+  config.entry = {
+    index: './index.js',
+  };
+  config.output = {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'twitter/dist'),
+    clean: {
+      dry: true,
+    },
+  };
+  config.devServer = {
+    contentBase: path.join(__dirname, 'twitter/dist'),
+    index: 'index.html',
+    // publicPath: 'twitter/dist',
+    host: '0.0.0.0',
+    port: 9000,
+  };
+  config.mode = 'development';
+  config.module.rules.push(
+    {
+      test: /\.(css)$/,
+      use: ['style-loader', 'css-loader'],
+      // use: [MiniCssExtractPlugin.loader, 'css-loader'],
+    },
+    {
+      test: /\.(scss)$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      // use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+    }
+  );
+}
+
+if (currentTask == 'build-twitter') {
+  config.entry = {
+    index: './index.js',
+  };
+  config.output = {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'twitter/dist'),
+    clean: {
+      dry: true,
+    },
+  };
+  config.mode = 'production';
+  config.module.rules.push(
+    {
+      test: /\.(css)$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader'],
+    },
+    {
+      test: /\.(scss)$/,
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+    }
+  );
+  config.plugins.push(
+    new MiniCssExtractPlugin({
+      // filename: 'styles.[contenthash].css',
+      filename: 'styles.[chunkhash].css',
+    })
+  );
+}
+
+module.exports = config;
